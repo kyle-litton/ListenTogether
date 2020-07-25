4969ec8b75674efcc252da1c7efcb6696421cb5e
James Kyle Litton <kyle.litton@rutgers.edu>
2020-07-24 20:22:33 -0400

    Updates

---
+++ precache-manifest.0267d801330162e23c112814c49be405.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c1915d32c4b1ec0492f5c04628ab990",
+    "url": "/ListenTogether/index.html"
+  },
+  {
+    "revision": "78ecb797e587aefce915",
+    "url": "/ListenTogether/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "165eb6c5a5a1ba07ad63",
+    "url": "/ListenTogether/static/css/main.35979893.chunk.css"
+  },
+  {
+    "revision": "78ecb797e587aefce915",
+    "url": "/ListenTogether/static/js/2.de343a17.chunk.js"
+  },
+  {
+    "revision": "ced42cf8fa64274e741ab6b27d08955e",
+    "url": "/ListenTogether/static/js/2.de343a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "165eb6c5a5a1ba07ad63",
+    "url": "/ListenTogether/static/js/main.621fcb7c.chunk.js"
+  },
+  {
+    "revision": "98fa736d0343d9dd496a",
+    "url": "/ListenTogether/static/js/runtime-main.8a5f5dac.js"
+  },
+  {
+    "revision": "eb9ba4d7935a8009df22d1db02b59dec",
+    "url": "/ListenTogether/static/media/Aromatron-Regular.eb9ba4d7.ttf"
+  },
+  {
+    "revision": "a3bb48b9d55755c3b46701a30ea63579",
+    "url": "/ListenTogether/static/media/devices.a3bb48b9.png"
+  },
+  {
+    "revision": "f4b3fff4bf4aff76c711dbdf0bc8ee5b",
+    "url": "/ListenTogether/static/media/logo.f4b3fff4.png"
+  },
+  {
+    "revision": "7b0b5f12bd213d5ffb804e3597f93f3d",
+    "url": "/ListenTogether/static/media/topLeft.7b0b5f12.png"
+  },
+  {
+    "revision": "81a215e07993be61b7c84516ea5bd881",
+    "url": "/ListenTogether/static/media/topRight.81a215e0.png"
+  }
+]);
